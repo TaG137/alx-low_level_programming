34786a653914795983d4e0aa15f9d19241cec831
Trivin <reckedricketyrickety@gmail.com>
2022-06-02 04:39:34 -0700

    idc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	puts ("\"Programming is like building a mutlilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a mutlilingual puzzle");
+	return (0);
 }
+
