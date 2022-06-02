90bc820f0b4664f691dedb73528cddba386cd07e
Trivin <reckedricketyrickety@gmail.com>
2022-06-02 05:00:47 -0700

    idc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the phrase "with proper grammar, but the outcome is a piece of art,"
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: 0 if exited successfully, else non-zero
  */
