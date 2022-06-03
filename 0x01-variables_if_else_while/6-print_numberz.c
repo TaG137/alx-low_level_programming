ada44be232d265847105384866cfd614821baf5c
Trivin <reckedricketyrickety@gmail.com>
2022-06-03 08:23:41 -0700

    idc

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers in base 10 and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
