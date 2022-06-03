79c111550f32be8ab2d56f9f22841760a1f2217f
Trivin <reckedricketyrickety@gmail.com>
2022-06-03 07:48:29 -0700

    idc

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase execpt for 
+ * main - Prints the alphabet in lowercase execpt for
  * 'e' and 'q'
  *
  * Return: Always 0 (Successful)
@@ -17,7 +17,7 @@ int main(void)
 			putchar(alpha);
 		} 
 		alpha++;
-	} 
+	}
 	putchar('\n');
 	return (0);
 }
