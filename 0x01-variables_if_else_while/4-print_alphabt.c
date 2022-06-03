c9d3b2b0159fdf900b92c790bfedc90b7bb9deed
Trivin <reckedricketyrickety@gmail.com>
2022-06-03 07:43:53 -0700

    idc

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase execpt for 
+ * 'e' and 'q'
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while(alpha <= 'z' )
+	{
+		if ( alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+		} 
+		alpha++;
+	} 
+	putchar('\n');
+	return (0);
+}
+
