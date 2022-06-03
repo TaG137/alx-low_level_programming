89d725fd73a67399a117d21efabf0cd0f4eaf4c2
Trivin <reckedricketyrickety@gmail.com>
2022-06-03 09:31:22 -0700

    idc

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  print the alphabet in reverse using putchar
+ * and have it in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main =(void)
+{
+	char alpha;
+
+	for (
