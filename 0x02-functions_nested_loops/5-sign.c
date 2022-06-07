bbe3831624689d02f73d563cd57b12902e8948ec
Trivin <reckedricketyrickety@gmail.com>
2022-06-07 10:04:47 -0700

    idc

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - prints whether a character is positive or
@@ -9,20 +9,18 @@
  */
 int print_sign(int n)
 {
-	
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
-	}
-	else if (n == 0)
+	} else if (n == 0)
 	{
-		putchar(0);
+		_putchar(0);
 		return (0);
-	}
-	else if (n < 0)
+	} else if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 	}
 	return (-1);
 }
+
