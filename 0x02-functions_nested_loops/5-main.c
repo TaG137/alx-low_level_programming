c5edadca264b5c95ff6e2da6452df120f28a51d8
Trivin <reckedricketyrickety@gmail.com>
2022-06-07 09:30:10 -0700

    idc

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+
+	_putchar(',');
+
+	_putchar(' ');
+
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	r = print_sign(0);
+
+	_putchar(',');
+
+	_putchar(' ');
+
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	r = print_sign(0xff);
+
+	_putchar(',');
+
+	_putchar(' ');
+
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	r = print_sign(-1);
+
+	_putchar(',');
+
+	_putchar(' ');
+
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_sign - prints whether a character is positive or
+ * negative.
+ * @n: The character to check
+ *
+ * Return: 1 if positive, 0 if zero or -1 if negative.
+ */
+int print_sign(int n)
+{
+	
+	if ((n < 0))
+	{
+		putchar('-');
+		return (-1);
+	}
+	else if ((n = 0))
+	{
+		putchar(0);
+		return (0);
+	}
+	else if ((n > 0))
+	{
+		putchar('+');
+	}
+	return (-1);
+}
