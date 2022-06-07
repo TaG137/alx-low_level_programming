2b03492fe680d50646e481d29f7df6780c680b13
Trivin <reckedricketyrickety@gmail.com>
2022-06-07 10:47:49 -0700

    idc

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of int
+ * @int: the interger needed to compute
+ *
+ * Return: Always 0 (success)
+ */
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
