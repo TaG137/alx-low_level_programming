7f85c775e5f495216af652662d3805aeeb7e8393
Trivin <reckedricketyrickety@gmail.com>
2022-06-07 11:43:11 -0700

    idc

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,8 @@
 int print_last_digit(int n)
 {
 	int a;
-	
-	if ( n < 0)
+
+	if (n < 0)
 	{
 		n = -n;
 	}
@@ -20,7 +20,7 @@ int print_last_digit(int n)
 	if (a < 0)
 	{
 		a = -a;
-	}	
+	}
 	_putchar(a + '0');
 	return (a);
 }
