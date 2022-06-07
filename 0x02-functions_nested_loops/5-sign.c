be203e44c75b57e3ab70f7f0630445ba7f387a6d
Trivin <reckedricketyrickety@gmail.com>
2022-06-07 09:53:07 -0700

    idc

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,19 +10,19 @@
 int print_sign(int n)
 {
 	
-	if ((n < 0))
+	if (n > 0)
 	{
-		putchar('-');
-		return (-1);
+		putchar('+');
+		return (1);
 	}
-	else if ((n = 0))
+	else if (n == 0)
 	{
 		putchar(0);
 		return (0);
 	}
-	else if ((n > 0))
+	else if (n < 0)
 	{
-		putchar('+');
+		putchar('-');
 	}
 	return (-1);
 }
