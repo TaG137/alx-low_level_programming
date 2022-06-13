6e1a04fedf5973b97d9756e5359cc1f4dc798117
Trivin <reckedricketyrickety@gmail.com>
2022-06-13 07:36:14 -0700

    new stuff

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	prinf("n=%d\n", n);
+	return(0);
+}
