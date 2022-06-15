b86881698f9055085ea71aeaec6cc0439bfb5607
TaG137 <105067138+TaG137@users.noreply.github.com>
2022-06-15 16:42:52 +0200

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two stings together
+* @dest: a string
+* @src: a string
+* Return: dest
+*/
+
+char *_strcat(char *src, char *dest)
+{
+  int len, i;
+  
+  while(len = 0; src[len] != '\0')
+  {
+    len++;
+    _putchar(src[len]);
+  }
+  return(dest);
+}
+    
+    
