dfde10409d9ebb4658faec7b53884a82cbf1955d
Trivin <reckedricketyrickety@gmail.com>
2022-06-27 04:22:25 -0700

    idc

---
+++ 0x0B-malloc_free/0-cerate_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array og characters and
+ * initializes them with a specific character
+ * @size: size of memory to print
+ * @c: the character to initialize
+ * Return: pointer to array (succesfful) or NULL
+ * (fail)
+ */
+
+char *create_array(unsigned int size; char c)
+{
+	char *pt;
+	unsigned int s = 0;
+
+	if (size == 0)
+		return (NULL);
+	pt = malloc(sizeof(char) * size);
+	while (s < size)
+	{
+		*(pt + s) = c;
+		s++;
+	}
+	*(pt + s) = '\0';
+	return(pt);
+}
+	
