32c42d8e588f42897af8a4cf6d1ff440b2bc0f88
TaG137 <105067138+TaG137@users.noreply.github.com>
2022-07-04 13:01:26 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,9 +3,9 @@
 
 /**
  * struct dog - a dog's basic info
- * @name: member one
- * @age: member two
- * @owner: member three
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
  * Description: Longer description
  */
@@ -17,9 +17,9 @@ struct dog
 };
 
 /**
- * dog - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
